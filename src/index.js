ff70a104186497ada6b3ff628fea6a915cb9d6f6
Mark Lee <mark@protopie.io>
2018-01-11 01:02:15 +0900

    완전 기초 설정

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './style.css';
+
+const App = () => <h1>Hello, parcel!</h1>;
+
+ReactDOM.render(<App />, document.getElementById('root'));
